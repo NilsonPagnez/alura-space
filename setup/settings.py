bf6183b16ea94280db57a4626f03092dcedc08e0
Nilson Pagnez <nilson.pagneto@gmail.com>
2023-07-27 11:01:47 -0300

    arquivo static com css e imagens adicionados

--- setup/settings.py
+++ setup/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static')
 ]
 
-STATIC_ROOT = 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
