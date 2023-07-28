e222210a4765ccb77342bf305398aa8518bab7ca
Nilson Pagnez <nilson.pagneto@gmail.com>
2023-07-28 11:57:11 -0300

    integrando back com front

--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from gallery.models import Fotografia
 
 def index(request):
-    return render(request, 'gallery/index.html')
+
+    fotografias = Fotografia.objects.all()
+
+    return render(request, 'gallery/index.html', {'cards': fotografias})
 
 
-def image(request):
-    return render(request, 'gallery/imagem.html')
+def image(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'gallery/imagem.html', {'fotografia': fotografia})
